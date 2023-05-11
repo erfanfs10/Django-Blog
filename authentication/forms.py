3d99f8e54acc6aba9690d9e7267f5ba417190163
erfan <mahdi.farahmandsafa@gmail.com>
2023-05-11 21:00:45 +0330

    locale updated

--- authentication/forms.py
+++ authentication/forms.py
@@ -1,9 +1,14 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-
+from django.utils.translation import gettext_lazy as _
+from django import forms
 
 class CustomUserCreationForm(UserCreationForm):
-
     class Meta(UserCreationForm.Meta):
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+        error_messages = {
+            "email": {
+                "unique": _("A user with that Email already exists."),
+            },
+        }
