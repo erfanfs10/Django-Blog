7c87579e3166045cfdadbf1bc4e7a80fc8bbfc40
erfan <mahdi.farahmandsafa@gmail.com>
2023-05-11 20:08:51 +0330

    send email

--- blog/settings.py
+++ blog/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 from django.utils.translation import gettext_lazy as _
-from .local_setting import SECRET_KEY, DEBUG
+from .local_setting import SECRET_KEY, DEBUG, EMAIL, APP_PASSWORD
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -156,7 +156,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'YOUR EMAIL ADDRESS'
-EMAIL_HOST_PASSWORD = "YOUR GOOGLE PASSWORD APP" 
+EMAIL_HOST_USER = EMAIL
+EMAIL_HOST_PASSWORD = APP_PASSWORD 
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
