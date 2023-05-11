b25ba21bfa40fa4383b0fc62926df6e5f2a88931
erfan <mahdi.farahmandsafa@gmail.com>
2023-05-11 23:52:29 +0330

    update locale

--- authentication/models.py
+++ authentication/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 from .manager import CustomUserManager
-
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
 
-    username = models.CharField(max_length=50, unique=True)
-    email = models.EmailField(unique=True)
+    username = models.CharField(_('username'), max_length=50, unique=True)
+    email = models.EmailField(_('Email'), unique=True)
 
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
