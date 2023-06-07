fd2dea9874c91a23e87e6cd0bdce532071e7c00d
erfan <mahdi.farahmandsafa@gmail.com>
2023-06-07 09:19:10 +0330

    debug

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,7 @@ from django.views.generic import TemplateView
 
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='resume.html')),
     path('blog/admin/', admin.site.urls),
     path('blog/', include('core.urls')),
     path('blog/auth/', include('authentication.urls')),
