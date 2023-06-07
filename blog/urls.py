aa72efcd7c4c6f17d8415f6d19f739467f2b8bd8
erfan <mahdi.farahmandsafa@gmail.com>
2023-06-07 09:17:02 +0330

    almost ready for development

---
+++ blog/urls.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('core.urls')),
+    path('auth/', include('authentication.urls')),
+    path('i18n/', include('django.conf.urls.i18n')),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/', include('api.urls'))
+
+
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
