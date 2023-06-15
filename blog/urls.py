bcefaf17c1185a49cb037dd308dea5b9b5ade847
erfan <mahdi.farahmandsafa@gmail.com>
2023-06-15 11:53:59 +0330

    FIX

--- blog/urls.py
+++ blog/urls.py
@@ -4,9 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
 
-
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='resume.html')),
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('auth/', include('authentication.urls')),
